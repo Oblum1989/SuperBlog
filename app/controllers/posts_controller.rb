a77306fe9d8b3f0692b8a775f00569685179679e
Oblum1989 <oscar.blum@gmail.com>
2019-05-19 15:18:38 -0500

    index and show views are added for the post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,12 +4,48 @@ class PostsController < ApplicationController
   end
 
   def show
-  
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
+    @categories = Category.all
+  end
+
+  def create  
+    @post = Post.new(post_params)
+    
+    if @post.save 
+      redirect_to post_path, notice: "El producto ha sido guardado exitosamente"
+    else
+      flash[:alert] = "El producto no ha sido guardado, por favor repetir la operacion"
+      render :new
+    end
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path    
+    else
+      render :edit  
+    end
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:user, :category, :title, :content, :image)
+    end
+
 end
